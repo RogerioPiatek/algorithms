f6157ec0a304c4d554a7668a1fd4e468a7fec1d6
Rogerio <rogeriopiatek@gmail.com>
2024-01-30 18:34:30 -0300

    added binary search algorithm

---
+++ binarySearch/index.ts
@@ -0,0 +1,34 @@
+/*
+Binary Search
+Takes an sorted array and an item.
+*/
+
+function binarySearch(list: number[], item: number) {
+  //these keep track of where in the list we search in
+  let low = 0;
+  let high = list.length - 1;
+
+  while (low <= high) {
+    //while not searched the entire list
+    let mid = low + high; //get the mid of the list
+    let guess = list[mid]; //set our guess
+    if (guess === item) {
+      //compares if our guess is what we want
+      return mid; //returns if true
+    }
+    //slices our list in half in case of guess > or < than item
+    if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return null;
+}
+
+//array needs to be sorted
+const myList: number[] = [1, 10, 20, 200, 1000];
+
+console.log(binarySearch(myList, 200));
+console.log(binarySearch(myList, 1));
+console.log(binarySearch(myList, 1000));
